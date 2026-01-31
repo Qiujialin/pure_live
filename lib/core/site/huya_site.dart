299dca6f5231d21c1fe236dd73403e726001a839
Qiujialin <78973190@qq.com>
2026-02-01 03:53:56 +0800

    fix(huya): 修复虎牙直播断流问题
    - 添加 WUP2 协议鉴权支持
    - 新增 tars_dart、crypto 依赖
    - 通过 getCdnTokenInfoEx 获取 CDN Token
    - 使用 buildAntiCode 计算签名
    - 升级 Kotlin 版本至 1.8.22
    - 更新 GitHub Actions 配置 (Flutter 3.10.6, Java 17)

--- lib/core/site/huya_site.dart
+++ lib/core/site/huya_site.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:math';
+import 'package:crypto/crypto.dart';
 import 'package:pure_live/common/index.dart';
 import 'package:http/http.dart' as http;
+import 'package:tars_dart/tars/net/base_tars_http.dart';
 
 import '../danmaku/huya_danmaku.dart';
 import '../interface/live_danmaku.dart';
 import '../interface/live_site.dart';
+import '../tars/huya_user_id.dart';
+import '../tars/get_cdn_token_ex_req.dart';
+import '../tars/get_cdn_token_ex_resp.dart';
 
 class HuyaSite implements LiveSite {
   @override
@@ -16,6 +22,21 @@ class HuyaSite implements LiveSite {
 
   @override
   LiveDanmaku getDanmaku() => HuyaDanmaku();
+
+  // WUP2 鉴权相关常量和客户端
+  static const String HYSDK_UA =
+      "HYSDK(Windows, 30000002)_APP(pc_exe&7060000&official)_SDK(trans&2.32.3.5646)";
+  static const String baseUrl = "https://m.huya.com/";
+  static Map<String, String> requestHeaders = {
+    'Origin': baseUrl,
+    'Referer': baseUrl,
+    'User-Agent': HYSDK_UA
+  };
+  final BaseTarsHttp tupClient = BaseTarsHttp(
+    "http://wup.huya.com",
+    "liveui",
+    headers: requestHeaders,
+  );
 
   static Future<dynamic> _getJson(String url) async {
     var resp = await http.get(
@@ -40,6 +61,10 @@ class HuyaSite implements LiveSite {
       if (response['status'] == 200) {
         Map data = response['data']['stream']['flv'];
 
+        // 获取 baseSteamInfoList 用于 WUP2 鉴权
+        List<dynamic> baseSteamInfoList =
+            response['data']['stream']['baseSteamInfoList'] ?? [];
+
         // 获取支持的分辨率
         Map<String, String> rates = {};
         for (var rate in data['rateArray']) {
@@ -50,12 +75,53 @@ class HuyaSite implements LiveSite {
         // 获取支持的线路
         links['原画'] = [];
         for (var item in data['multiLine']) {
-          String url = (item['url']).replaceAll('http://', 'https://');
-          links['原画']?.add(url);
-          for (var name in rates.keys) {
-            links[name] ??= [];
-            links[name]?.add(
-                url.replaceAll('imgplus.flv', 'imgplus${rates[name]!}.flv'));
+          String cdnType = item['cdnType']?.toString() ?? '';
+
+          // 从 baseSteamInfoList 中查找对应的流信息
+          var streamInfo = baseSteamInfoList.firstWhere(
+            (e) => e['sCdnType'] == cdnType,
+            orElse: () => null,
+          );
+
+          if (streamInfo != null) {
+            String streamName = streamInfo['sStreamName']?.toString() ?? '';
+            String sFlvUrl = streamInfo['sFlvUrl']?.toString() ?? '';
+            int presenterUid = 0;
+
+            // 获取 presenterUid (lChannelId)
+            var lChannelId = streamInfo['lChannelId'];
+            if (lChannelId is String) {
+              presenterUid = int.tryParse(lChannelId) ?? 0;
+            } else if (lChannelId is int) {
+              presenterUid = lChannelId;
+            }
+
+            if (streamName.isNotEmpty && sFlvUrl.isNotEmpty) {
+              try {
+                // 通过 WUP2 获取 token
+                String antiCode = await getCdnTokenInfoEx(streamName);
+                // 构建签名后的 antiCode
+                antiCode = buildAntiCode(streamName, presenterUid, antiCode);
+                // 构建最终 URL
+                String finalUrl =
+                    '$sFlvUrl/$streamName.flv?$antiCode&codec=264';
+                finalUrl = finalUrl.replaceAll('http://', 'https://');
+
+                links['原画']?.add(finalUrl);
+
+                // 添加其他清晰度
+                for (var name in rates.keys) {
+                  links[name] ??= [];
+                  String rateUrl =
+                      '$sFlvUrl/$streamName${rates[name]}.flv?$antiCode&codec=264';
+                  rateUrl = rateUrl.replaceAll('http://', 'https://');
+                  links[name]?.add(rateUrl);
+                }
+              } catch (e) {
+                log('WUP2 token error for $cdnType: $e',
+                    name: 'HuyaApi.getLiveStream');
+              }
+            }
           }
         }
       }
@@ -239,5 +305,89 @@ class HuyaSite implements LiveSite {
       return list;
     }
     return list;
+  }
+
+  /// 通过 WUP2 协议获取 CDN Token
+  Future<String> getCdnTokenInfoEx(String stream) async {
+    var func = "getCdnTokenInfoEx";
+    var tid = HuyaUserId();
+    tid.sHuYaUA = "pc_exe&7060000&official";
+    var tReq = GetCdnTokenExReq();
+    tReq.tId = tid;
+    tReq.sStreamName = stream;
+    var resp = await tupClient.tupRequest(func, tReq, GetCdnTokenExResp());
+    return resp.sFlvToken;
+  }
+
+  /// 构建 antiCode 签名
+  /// [stream] streamName
+  /// [presenterUid] 主播 UID (lChannelId)
+  /// [antiCode] 从 WUP2 获取的原始 token
+  String buildAntiCode(String stream, int presenterUid, String antiCode) {
+    var mapAnti = Uri(query: antiCode).queryParametersAll;
+    if (!mapAnti.containsKey("fm")) {
+      return antiCode;
+    }
+
+    var ctype = mapAnti["ctype"]?.first ?? "huya_pc_exe";
+    var platformId = int.tryParse(mapAnti["t"]?.first ?? "0");
+
+    bool isWap = platformId == 103;
+    var clacStartTime = DateTime.now().millisecondsSinceEpoch;
+
+    // 计算 seqId
+    var seqId = presenterUid + clacStartTime;
+
+    // 计算 secretHash = MD5(seqId|ctype|platformId)
+    final secretHash =
+        md5.convert(utf8.encode('$seqId|$ctype|$platformId')).toString();
+
+    // rotl64 位旋转
+    final convertUid = rotl64(presenterUid);
+    final calcUid = isWap ? presenterUid : convertUid;
+
+    // 解码 fm 获取 secretPrefix
+    final fm = Uri.decodeComponent(mapAnti['fm']!.first);
+    final secretPrefix = utf8.decode(base64.decode(fm)).split('_').first;
+    var wsTime = mapAnti['wsTime']!.first;
+
+    // 计算 wsSecret
+    final secretStr =
+        '${secretPrefix}_${calcUid}_${stream}_${secretHash}_$wsTime';
+    final wsSecret = md5.convert(utf8.encode(secretStr)).toString();
+
+    // 生成 UUID
+    final rnd = Random();
+    final ct =
+        ((int.parse(wsTime, radix: 16) + rnd.nextDouble()) * 1000).toInt();
+    final uuid =
+        (((ct % 1e10) + rnd.nextDouble()) * 1e3 % 0xffffffff).toInt().toString();
+
+    // 组装结果
+    final Map<String, dynamic> antiCodeRes = {
+      'wsSecret': wsSecret,
+      'wsTime': wsTime,
+      'seqid': seqId,
+      'ctype': ctype,
+      'ver': '1',
+      'fs': mapAnti['fs']!.first,
+      'fm': Uri.encodeComponent(mapAnti['fm']!.first),
+      't': platformId,
+    };
+    if (isWap) {
+      antiCodeRes.addAll({'uid': presenterUid, 'uuid': uuid});
+    } else {
+      antiCodeRes['u'] = convertUid;
+    }
+
+    return antiCodeRes.entries.map((e) => '${e.key}=${e.value}').join('&');
+  }
+
+  /// 64位左旋转
+  int rotl64(int t) {
+    final low = t & 0xFFFFFFFF;
+    final rotatedLow = ((low << 8) | (low >> 24)) & 0xFFFFFFFF;
+    final high = t & ~0xFFFFFFFF;
+    return high | rotatedLow;
   }
 }
